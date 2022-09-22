3e4ead1d041967fb8e75fdddb0e4add5234ca29e
Iván Camilo Sanabria <icsanabriar@googlemail.com>
2022-09-22 19:59:26 +0200

    FUN-9: Add regex practices.

---
+++ src/main/java/com/hacker/rank/regex/easy/Specific.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2022 Iván Camilo Sanabria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hacker.rank.regex.easy;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Class that is executed in hacker rank website as solution.
+ *
+ * @author Iván Camilo Sanabria (icsanabriar@googlemail.com)
+ * @since  1.3.0
+ */
+public class Specific {
+
+    /**
+     * Main function provided by hacker rank website.
+     *
+     * @param args Arguments of the program.
+     */
+    public static void main(String[] args) {
+        Specific_Test tester = new Specific_Test();
+        tester.checker("^[123][120][xs0][30Aa][xsu][.,]$");
+    }
+}
+
+/**
+ * Given test class on hacker rank website.
+ */
+class Specific_Test {
+
+    /**
+     * Checker of the given regex pattern.
+     *
+     * @param Regex_Pattern Pattern to validate.
+     */
+    void checker(String Regex_Pattern){
+
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }
+
+}
