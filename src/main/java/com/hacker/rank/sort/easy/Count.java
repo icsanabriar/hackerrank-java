6b45521aa4bf4ff3affc9d3a7f5d2a2ca968d398
Iván Camilo Sanabria <icsanabriar@googlemail.com>
2022-10-20 20:14:31 +0200

    FUN-10: Add sort practices.

---
+++ src/main/java/com/hacker/rank/sort/easy/Count.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2022 Iván Camilo Sanabria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hacker.rank.sort.easy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Class that is executed in hacker rank website as solution.
+ *
+ * @author Iván Camilo Sanabria (icsanabriar@googlemail.com)
+ * @since  1.3.0
+ */
+public class Count {
+
+    /**
+     * Regex used to process input of the program.
+     */
+    private static final String REGEX = "\\s+$";
+
+    /**
+     * Separator of values in same line.
+     */
+    private static final String SEPARATOR = " ";
+
+    /**
+     * Replacement of value inline.
+     */
+    private static final String REPLACEMENT = "";
+
+    /**
+     * Array to sort using counting sort.
+     *
+     * @param arr Array to sort.
+     * @param n   Size of the given array
+     * @return Sorted array after counting sort.
+     */
+    private static List<Integer> countingSort(final List<Integer> arr, final int n) {
+
+        final int[] counters = new int[100];
+
+        for (Integer value : arr) {
+            counters[value]++;
+        }
+
+        final List<Integer> results = new ArrayList<>(n);
+
+        for (int i = 0; i < counters.length; i++) {
+            for (int j = 0; j < counters[i]; j++) {
+                results.add(i);
+            }
+        }
+
+        return results;
+    }
+
+    /**
+     * Main function provided by hacker rank website.
+     *
+     * @param args Arguments of the program.
+     * @throws IOException Thrown when the application is not able to read or write data in the OUTPUT_PATH.
+     */
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            final int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")))) {
+
+                final List<Integer> arr = Stream.of(bufferedReader.readLine()
+                        .replaceAll(REGEX, REPLACEMENT)
+                        .split(SEPARATOR))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                final List<Integer> result = countingSort(arr, n);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n");
+            }
+        }
+    }
+}
---
+++ src/test/java/com/hacker/rank/sort/easy/CountTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2022 Iván Camilo Sanabria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hacker.rank.sort.easy;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Class to handle the test cases given on hacker rank website.
+ *
+ * @author Iván Camilo Sanabria (icsanabriar@googlemail.com)
+ * @since  1.3.0
+ */
+public class CountTest {
+
+    /**
+     * Define line separator for test cases.
+     */
+    private static final String SEPARATOR = System.lineSeparator();
+
+    @Test
+    @SuppressWarnings("AccessStaticViaInstance")
+    public void given_test_case() throws IOException {
+
+        final String[] args = {};
+        final InputStream sysInBackup = System.in;
+
+        final String input = "100" + SEPARATOR +
+                "63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 " +
+                "59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 " +
+                "99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33 " + SEPARATOR;
+
+        final String expectedOutput = "1 1 3 3 6 8 9 9 10 12 13 16 16 18 20 21 21 22 23 24 25 25 25 27 27 30 30 32 " +
+                "32 32 33 33 33 34 39 39 40 40 41 42 43 44 44 46 46 48 50 53 56 56 57 59 60 61 63 65 67 67 68 69 69 " +
+                "69 70 70 73 73 74 75 75 76 78 78 79 79 80 81 81 82 83 83 84 85 86 86 87 87 89 89 89 90 90 91 92 94 " +
+                "95 96 98 98 99 99" + SEPARATOR;
+
+        final ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        final Count instance = new Count();
+        instance.main(args);
+
+        final File file = new File(System.getenv("OUTPUT_PATH"));
+        final Scanner sc = new Scanner(file);
+
+        final String[] lines = expectedOutput.split("\\r?\\n");
+
+        int index = 0;
+
+        while (sc.hasNextLine()) {
+            assertEquals(lines[index], sc.nextLine());
+            index++;
+        }
+
+        sc.close();
+
+        System.setIn(sysInBackup);
+    }
+
+}
