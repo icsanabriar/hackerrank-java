d047b5d853498d2ff463be326a3614b95384ea0b
Iván Camilo Sanabria <icsanabriar@googlemail.com>
2022-10-21 12:48:17 +0200

    FUN-9: Add regex practices.

---
+++ src/test/java/com/hacker/rank/regex/medium/BackTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2022 Iván Camilo Sanabria.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hacker.rank.regex.medium;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Class to handle the test cases given on hacker rank website.
+ *
+ * @author Iván Camilo Sanabria (icsanabriar@googlemail.com)
+ * @since  1.3.0
+ */
+public class BackTest {
+
+    /**
+     * Define line separator for test cases.
+     */
+    private static final String SEPARATOR = System.lineSeparator();
+
+    @Test
+    @SuppressWarnings("AccessStaticViaInstance")
+    public void given_test_case() {
+
+        final String[] args = {};
+        final InputStream sysInBackup = System.in;
+
+        final String input = "12-34-56-87" + SEPARATOR;
+        final String expectedOutput = "true" + SEPARATOR;
+
+        final ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        final ByteArrayOutputStream controllerOut = new ByteArrayOutputStream();
+
+        System.setIn(in);
+        System.setOut(new PrintStream(controllerOut));
+
+        final Back instance = new Back();
+        instance.main(args);
+
+        assertEquals(expectedOutput, controllerOut.toString());
+
+        System.setIn(sysInBackup);
+    }
+
+}
